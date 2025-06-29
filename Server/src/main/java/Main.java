e397720cb90492248903d40b0e2599e2669f531c
Tastekin_ <salih.tastekin@web.de>
2025-06-29 18:41:51 +0200

    LAST LAST Changes

--- Server/src/main/java/Main.java
+++ Server/src/main/java/Main.java
@@ -20,8 +20,6 @@ import java.sql.SQLException;
 import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
-import java.util.Scanner;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
